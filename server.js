3195fe931594e9e7ff6a8d683616d71bd48607fb
nguyenkhacdat1402 <nguyenkhacdatt396@gmail.com>
2026-08-16 14:21:21 +0700

    v3

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.json());
 
 // ====== CẤU HÌNH — thay bằng giá trị thật của bạn ======
 const VERIFY_TOKEN = "testautoreplyfacebook";       // Phải khớp với ô "Verify Token" trên Facebook App
-const PAGE_ACCESS_TOKEN = "PAGE_ACCESS_TOKEN_CUA_BAN"; // Lấy ở bước 3 trong hướng dẫn
+const PAGE_ACCESS_TOKEN = "https://testautoreply.onrender.com/"; // Lấy ở bước 3 trong hướng dẫn
 const PORT = process.env.PORT || 3000;
 app.get("/", (req, res) => {
   res.json("Bot tự động trả lời tin nhắn Facebook Page qua Messenger Platform");
